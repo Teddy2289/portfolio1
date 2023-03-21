c07d45a81da8216a5f86ce044836f0e94f332dce
Rinah RALAIVAO <teddyrinah@gmail.com>
2023-03-21 08:38:14 +0100

    teste line icone

--- src/components/education/Card.jsx
+++ src/components/education/Card.jsx
@@ -7,7 +7,7 @@ import {BsBriefcaseFill} from 'react-icons/bs'
 function Card(props) {
   return (
     <TimeLine>
-      {props.icon}
+     <i className={props.icon}></i>
       <span className="timeline_date">{props.year}</span>
       <h3 className="timeline_title">{props.title}</h3>
       <p className="timeline_text">{props.desc}</p>
